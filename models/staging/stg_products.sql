a250a2cb39c0d724008f9ba2139817aa2618c04b
Kathrine Sofie Rasmussen <rasmussen.kathrine20@gmail.com>
2026-02-08 15:31:40 +0000

    Column renames and casting of data types

--- models/staging/stg_products.sql
+++ models/staging/stg_products.sql
@@ -1,2 +1,35 @@
+with source as (
+
 select *
 from {{ source('bike_sales_src', 'products') }}
+), 
+
+renamed_products as (
+
+    select
+        PRODUCTKEY as product_id,
+        PRODUCTSUBCATEGORYKEY as product_sub_category_id,
+        PRODUCTNAME as product_name,
+        STANDARDCOST as standard_cost, 
+        COLOR as color,
+        SAFETYSTOCKLEVEL as safety_stock_level,
+        LISTPRICE as list_price,
+        SIZERANGE as size_range,
+        WEIGHT as weight,
+        DAYSTOMANUFACTURE as days_to_manufacture,
+        PRODUCTLINE as product_line,
+        DEALERPRICE as dealer_price,
+        CLASS as class,
+        MODELNAME as model_name,
+        DESCRIPTION as description,
+        STARTDATE::date as start_date, 
+        ENDDATE::date as end_date,
+        STATUS as status,
+        CURRENTSTOCK as current_stock
+    from source
+
+)
+
+select *
+from renamed_products
+
